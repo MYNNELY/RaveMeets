1557ae160fede85370065af60306e544daed52a1
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-08-27 22:40:54 -0700

    Changed a tags to Material-UI Link tags.

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable require-jsdoc */
+import React from 'react';
+import {Box, Grid, Link} from '@material-ui/core';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Box className="nav-bar-container" component="div">
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid container item xs={2} spacing={1} justifyContent="center">
+            <h1>RAVEmeets</h1>
+          </Grid>
+          <Grid container item xs={2} spacing={1} justifyContent="center">
+            <Link href="#" className="nav-bar" color="#FFF">
+              Events
+            </Link>
+          </Grid>
+          <Grid container item xs={2} spacing={1} justifyContent="center">
+            <Link href="#" className="nav-bar" color="#FFF">
+              Groups
+            </Link>
+          </Grid>
+          <Grid container item xs={2} spacing={1} justifyContent="center">
+            <Link href="#" className="nav-bar" color="#FFF">
+              My Profile
+            </Link>
+          </Grid>
+          <Grid container item xs={2} spacing={1} justifyContent="center">
+            <input type="text" className="nav-bar" placeholder="Search" />
+          </Grid>
+        </Grid>
+      </Box>
+    );
+  }
+}
+
+export default NavBar;
