a1ea8e5966c881ee6f9e0267eb5eef5afb2c16eb
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-08-30 13:52:56 -0700

    my changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,15 @@
-const test = () => {
-  return 'Hello World';
-};
+import React from 'react';
+// import NavBar from './NavBar/NavBar.jsx';
+// import SearchAppBar from './NavBar/SearchAppBar.jsx';
+import GroupListPage from './GroupListPage/GroupListPage.jsx';
 
+const test = () => {
+  // return 'Hello World';
+  return (
+    <div>
+      <GroupListPage />
+    </div>
+  );
+};
 
 export default test;
