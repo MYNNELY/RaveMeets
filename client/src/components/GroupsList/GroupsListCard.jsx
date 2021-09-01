24ba7b4ffc7bad1d44337eb36d7366b35333f2bc
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-09-01 14:52:38 -0700

    Styling for Groups List

--- client/src/components/GroupsList/GroupsListCard.jsx
+++ client/src/components/GroupsList/GroupsListCard.jsx
@@ -8,23 +8,74 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     marginTop: '5px',
   },
+  groupsListInfo: {
+    position: 'relative',
+
+    color: '#FFF',
+    maxHeight: '356px',
+    overflow: 'hidden',
+  },
+  mainImage: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+  topLeft: {
+    position: 'absolute',
+    top: '30px',
+    left: '30px',
+  },
 }));
 
 const GroupsListCard = () => {
   const classes = useStyles();
   return (
-    <Grid container spacing={5} className={classes.groupsListCard}>
-      <Grid item xs={5}>
-        <h3>Group 1 Name</h3>
-        <h3>Event 1 Name</h3>
-        <p>Event Description</p>
-        <p>Event Location</p>
-        <p>main pic</p>
+    <>
+      <Grid container spacing={5} className={classes.groupsListCard} key="1">
+        <Grid item xs={5} className={classes.groupsListInfo}>
+          <img src="https://static01.nyt.com/images/2020/03/10/arts/10virus-coachella1/10virus-coachella1-mediumSquareAt3X.jpg" className={classes.mainImage} />
+          <div className={classes.topLeft}>
+            <h3>Group 1 Name</h3>
+            <h3>Event 1 Name</h3>
+            <p>Event Description</p>
+            <p>Event Location</p>
+          </div>
+        </Grid>
+        <Grid item className={classes.groupsListGallery}>
+          <GroupsListGallery />
+        </Grid>
       </Grid>
-      <Grid item className={classes.groupsListGallery}>
-        <GroupsListGallery />
+
+      <Grid container spacing={5} className={classes.groupsListCard} key="2">
+        <Grid item xs={5} className={classes.groupsListInfo}>
+          <img src="https://static01.nyt.com/images/2020/03/10/arts/10virus-coachella1/10virus-coachella1-mediumSquareAt3X.jpg" className={classes.mainImage} />
+          <div className={classes.topLeft}>
+            <h3>Group 1 Name</h3>
+            <h3>Event 2 Name</h3>
+            <p>Event Description</p>
+            <p>Event Location</p>
+          </div>
+        </Grid>
+        <Grid item className={classes.groupsListGallery}>
+          <GroupsListGallery />
+        </Grid>
       </Grid>
-    </Grid>
+
+      <Grid container spacing={5} className={classes.groupsListCard} key="3">
+        <Grid item xs={5} className={classes.groupsListInfo}>
+          <img src="https://static01.nyt.com/images/2020/03/10/arts/10virus-coachella1/10virus-coachella1-mediumSquareAt3X.jpg" className={classes.mainImage} />
+          <div className={classes.topLeft}>
+            <h3>Group 2 Name</h3>
+            <h3>Event 3 Name</h3>
+            <p>Event Description</p>
+            <p>Event Location</p>
+          </div>
+        </Grid>
+        <Grid item className={classes.groupsListGallery}>
+          <GroupsListGallery />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
--- client/src/components/GroupsList/GroupsListGallery.jsx
+++ client/src/components/GroupsList/GroupsListGallery.jsx
@@ -76,14 +76,6 @@ const GroupsListGallery = () => {
   {
     img: 'https://material-ui.com/static/images/image-list/breakfast.jpg',
     title: 'Breakfast',
-  },
-  {
-    img: 'https://material-ui.com/static/images/image-list/camera.jpg',
-    title: 'Camera',
-  },
-  {
-    img: 'https://material-ui.com/static/images/image-list/olive.jpg',
-    title: 'Olive oil',
   }];
   return (
     <ImageList rowHeight={160} className={classes.imageList} cols={4}>
