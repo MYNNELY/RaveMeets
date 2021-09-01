422c27efb49b09ec7058d8fdc112ef2bd74d2044
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-09-01 13:37:53 -0700

    groups list gallery styling

--- client/src/components/GroupsList/GroupsListCard.jsx
+++ client/src/components/GroupsList/GroupsListCard.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import GroupsListGallery from './GroupsListGallery.jsx';
+import {Grid} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  groupsListCard: {
+    justifyContent: 'center',
+    marginTop: '5px',
+  },
+}));
 
 const GroupsListCard = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h3>Group 1 Name</h3>
-      <h3>Event 1 Name</h3>
-      <p>Event Description</p>
-      <p>Event Location</p>
-      <p>main pic</p>
-      <GroupsListGallery />
-    </div>
+    <Grid container spacing={5} className={classes.groupsListCard}>
+      <Grid item xs={5}>
+        <h3>Group 1 Name</h3>
+        <h3>Event 1 Name</h3>
+        <p>Event Description</p>
+        <p>Event Location</p>
+        <p>main pic</p>
+      </Grid>
+      <Grid item className={classes.groupsListGallery}>
+        <GroupsListGallery />
+      </Grid>
+    </Grid>
   );
 };
 
