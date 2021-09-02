db48252d4eb1cf51b034d565d7accd42b5284c29
Jesse <jessemchung1@gmail.com>
2021-09-02 11:34:03 -0700

    done

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ const authRoutes = require('./routes/auth.js');
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_AUTH_TOKEN;
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
-const twilioClient = require('twilio')(accountSid, authToken);
+// require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+// const twilioClient = require('twilio')(accountSid, authToken);
 
 app.use(cors());
 app.use(express.json());
@@ -19,29 +19,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/auth', authRoutes);
 
 
-app.post('/', (req, res) => {
-  const { message, user: sender, type, members } = req.body;
+// app.post('/', (req, res) => {
+//   const { message, user: sender, type, members } = req.body;
 
-  if (type === 'message.new') {
-    members
-      .filter((member) => member.user_id !== sender.id)
-      .forEach(({ user }) => {
-        if (!user.online) {
-          twilioClient.messages.create({
-            body: `You have a new message from ${message.user.fullName} - ${message.text}`,
-            messagingServiceSid: messagingServiceSid,
-            to: user.phoneNumber
-          })
-            .then(() => console.log('Message sent!'))
-            .catch((err) => console.log(err));
-        }
-      })
+//   if (type === 'message.new') {
+//     members
+//       .filter((member) => member.user_id !== sender.id)
+//       .forEach(({ user }) => {
+//         if (!user.online) {
+//           twilioClient.messages.create({
+//             body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+//             messagingServiceSid: messagingServiceSid,
+//             to: user.phoneNumber
+//           })
+//             .then(() => console.log('Message sent!'))
+//             .catch((err) => console.log(err));
+//         }
+//       })
 
-    return res.status(200).send('Message sent!');
-  }
+//     return res.status(200).send('Message sent!');
+//   }
 
-  return res.status(200).send('Not a new message request');
-});
+//   return res.status(200).send('Not a new message request');
+// });
 
 
 app.use('/', express.static('./client/dist'));
