405283d48a6f63dfdc30583ae8c6696d4aa1d965
Shirley Zhang <shirley.a.zhang@gmail.com>
2021-09-02 23:06:57 -0700

    passing group info into groups card

--- client/src/components/GroupsList/GroupsListCard.jsx
+++ client/src/components/GroupsList/GroupsListCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import GroupsListGallery from './GroupsListGallery.jsx';
 import {Grid} from '@material-ui/core';
@@ -30,55 +31,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const GroupsListCard = () => {
+const GroupsListCard = ({group}) => {
   const classes = useStyles();
+  const {group_id: groupId, group_name: groupName} = group;
+
   return (
-    <>
-      <Grid container spacing={1} className={classes.groupsListCard} key="1">
-        <Grid item xs={5} className={classes.groupsListInfo}>
-          <img src="https://static01.nyt.com/images/2020/03/10/arts/10virus-coachella1/10virus-coachella1-mediumSquareAt3X.jpg" className={classes.mainImage} />
-          <div className={classes.topLeft}>
-            <h3>Group 1 Name</h3>
-            <h3>Event 1 Name</h3>
-            <p>Event Description</p>
-            <p>Event Location</p>
-          </div>
-        </Grid>
-        <Grid item className={classes.groupsListGallery}>
-          <GroupsListGallery />
-        </Grid>
+    <Grid container spacing={1} className={classes.groupsListCard} key="1">
+      <Grid item xs={5} className={classes.groupsListInfo}>
+        <img src="https://static01.nyt.com/images/2020/03/10/arts/10virus-coachella1/10virus-coachella1-mediumSquareAt3X.jpg" className={classes.mainImage} />
+        <div className={classes.topLeft}>
+          <h3>{groupName}</h3>
+          <h3>Event 1 Name</h3>
+          <p>Event Description</p>
+          <p>Event Location</p>
+        </div>
       </Grid>
-
-      <Grid container spacing={1} className={classes.groupsListCard} key="2">
-        <Grid item xs={5} className={classes.groupsListInfo}>
-          <img src="https://static01.nyt.com/images/2020/03/10/arts/10virus-coachella1/10virus-coachella1-mediumSquareAt3X.jpg" className={classes.mainImage} />
-          <div className={classes.topLeft}>
-            <h3>Group 1 Name</h3>
-            <h3>Event 2 Name</h3>
-            <p>Event Description</p>
-            <p>Event Location</p>
-          </div>
-        </Grid>
-        <Grid item className={classes.groupsListGallery}>
-          <GroupsListGallery />
-        </Grid>
+      <Grid item className={classes.groupsListGallery}>
+        <GroupsListGallery />
       </Grid>
-
-      <Grid container spacing={1} className={classes.groupsListCard} key="3">
-        <Grid item xs={5} className={classes.groupsListInfo}>
-          <img src="https://static01.nyt.com/images/2020/03/10/arts/10virus-coachella1/10virus-coachella1-mediumSquareAt3X.jpg" className={classes.mainImage} />
-          <div className={classes.topLeft}>
-            <h3>Group 2 Name</h3>
-            <h3>Event 3 Name</h3>
-            <p>Event Description</p>
-            <p>Event Location</p>
-          </div>
-        </Grid>
-        <Grid item className={classes.groupsListGallery}>
-          <GroupsListGallery />
-        </Grid>
-      </Grid>
-    </>
+    </Grid>
   );
 };
 
