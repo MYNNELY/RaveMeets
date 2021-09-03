eab2a905783464074395294ab88b5d8405980d2d
Cody Szeto <codyszet@gmail.com>
2021-09-02 21:11:08 -0700

    Added function file

--- client/src/components/EventPage/EventPage.jsx
+++ client/src/components/EventPage/EventPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Banner from './Banner.jsx';
 import CreateGroup from './CreateGroup.jsx';
 import Description from './Description.jsx';
 import Title from './Title.jsx';
 import {Container} from '@material-ui/core';
 
+import {getEvent} from './functions';
+
 
 const EventPage = () => {
+  const [event, setEvent] = useState(null);
+
   return (
     <Container style={{maxWidth: '1500px'}}>
       <Banner />
