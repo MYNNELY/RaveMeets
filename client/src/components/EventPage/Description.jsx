c9d573c5325bdff16450efb877c4145652a60742
Cody Szeto <codyszet@gmail.com>
2021-09-26 22:56:16 -0700

    Implemented prop validation for Description component

--- client/src/components/EventPage/Description.jsx
+++ client/src/components/EventPage/Description.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyledDescription} from './Styled';
+import PropTypes from 'prop-types';
 
 const Description = ({description}) => {
   return (
@@ -7,6 +8,10 @@ const Description = ({description}) => {
       {description}
     </StyledDescription>
   );
+};
+
+Description.propTypes = {
+  description: PropTypes.string,
 };
 
 export default Description;
