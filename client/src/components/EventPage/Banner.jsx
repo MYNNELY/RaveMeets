4276742f6067865b6e505a196fd1b3450071c9b6
Cody Szeto <codyszet@gmail.com>
2021-09-27 00:05:48 -0700

    Added functionality to view entire banner image

--- client/src/components/EventPage/Banner.jsx
+++ client/src/components/EventPage/Banner.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyledBannerContainer, StyledBannerImage} from './Styled';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import PropTypes from 'prop-types';
 
 const Banner = ({url}) => {
+  const [open, setOpen] = useState(null);
+  const openBanner = () => setOpen(true);
+  const  closeBanner = () => setOpen(false);
   return (
-    <StyledBannerContainer>
-      <StyledBannerImage src={url} alt='Banner image for event' />
-    </StyledBannerContainer>
+    <>
+      <StyledBannerContainer>
+        <StyledBannerImage src={url} alt='Banner image for event' />
+      </StyledBannerContainer>
+      <Modal
+        open = {open}
+        onClose = {closeBanner}
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div>
+            <StyledBannerImage src={url} alt='Banner image for event' />
+          </div>
+        </Fade>
+      </Modal>
+    </>
   );
 };
 
