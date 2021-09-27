46db33964afb71f92356e18b139b73057301ac82
Cody Szeto <codyszet@gmail.com>
2021-09-26 23:14:53 -0700

    Added prop validation

--- client/src/components/EventPage/Title.jsx
+++ client/src/components/EventPage/Title.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import {getTime} from './functions';
 import {StyledSpan} from './Styled';
 import {priceRating} from './functions.js';
+import PropTypes from 'prop-types';
 
 const Title = ({info}) => {
-  const {name='', start_date='', end_date='', venue={}, genres=[], price=null} = info;
+  const {
+    name = '',
+    start_date = '',
+    end_date = '',
+    venue = {},
+    genres = [],
+    price = null,
+  } = info;
   return (
     <div style={{
       display: 'flex',
@@ -17,12 +25,16 @@ const Title = ({info}) => {
         letterSpacing: '0.2rem'}}>{name}</div>
       <div style={{marginBottom: '0.3em'}}>{getTime(start_date, end_date)}</div>
       <div style={{marginBottom: '0.3em'}}>{venue.name} - {venue.address}</div>
-      <div style={{marginBottom: '0.3em'}}>{genres.map((genre) => {
-        return <StyledSpan>{genre.genre_name}</StyledSpan>
+      <div style={{marginBottom: '0.3em'}}>{genres.map((genre, index) => {
+        return <StyledSpan key={index}>{genre.genre_name}</StyledSpan>;
       })}</div>
       <div style={{marginBottom: '1.5em'}}>{priceRating(price)}</div>
     </div>
   );
+};
+
+Title.propTypes = {
+  info: PropTypes.object,
 };
 
 export default Title;
