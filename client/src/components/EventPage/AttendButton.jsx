9381e98a3b49217a6abfdc7f9e8572f17557e1cb
Cody Szeto <codyszet@gmail.com>
2021-09-27 00:29:02 -0700

    Initialized functions to be used on AttendButton component

--- client/src/components/EventPage/AttendButton.jsx
+++ client/src/components/EventPage/AttendButton.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Button} from '@material-ui/core';
 
 const AttendButton = () => {
+  const username = localStorage.getItem('username');
+
+  // handles clicking to attend event
+  const attendEvent = () => {};
+
+  // handles cancelling attendance to an event
+  const cancelAttendEvent = () => {};
   return (
-    <div></div>
+    <Button
+      variant='contained'
+      disabled={!username}
+    >
+      Attending
+    </Button>
   );
 };
 
