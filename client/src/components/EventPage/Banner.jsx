6d443f49cdc19f66c7e133d47050630bd514bafc
Cody Szeto <codyszet@gmail.com>
2021-09-27 17:04:48 -0700

    Added functionality to open banner image

--- client/src/components/EventPage/Banner.jsx
+++ client/src/components/EventPage/Banner.jsx
@@ -1,14 +1,24 @@
 import React, {useState} from 'react';
 import {StyledBannerContainer, StyledBannerImage} from './Styled';
+import {makeStyles} from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import PropTypes from 'prop-types';
 
+const useStyles = makeStyles({
+  modal: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
 const Banner = ({url}) => {
+  const classes = useStyles();
   const [open, setOpen] = useState(null);
   const openBanner = () => setOpen(true);
-  const  closeBanner = () => setOpen(false);
+  const closeBanner = () => setOpen(false);
   return (
     <>
       <StyledBannerContainer onClick={() => openBanner()}>
@@ -17,13 +27,15 @@ const Banner = ({url}) => {
       <Modal
         open = {open}
         onClose = {closeBanner}
+        onClick = {closeBanner}
         BackdropComponent={Backdrop}
+        className={classes.modal}
         BackdropProps={{
           timeout: 500,
         }}
       >
         <Fade in={open}>
-          <div>
+          <div style={{width: '70%'}}>
             <StyledBannerImage src={url} alt='Banner image for event' />
           </div>
         </Fade>
