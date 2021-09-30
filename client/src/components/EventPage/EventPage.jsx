1f614677bf7d676a7c6c3e7d5ade4e134d96ff5c
Cody Szeto <codyszet@gmail.com>
2021-09-29 23:06:23 -0700

    Refactored event page

--- client/src/components/EventPage/EventPage.jsx
+++ client/src/components/EventPage/EventPage.jsx
@@ -1,15 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import Banner from './Banner.jsx';
-import CreateGroup from './CreateGroup.jsx';
 import Description from './Description.jsx';
-import SideInfo from './SideInfo.jsx';
 import Title from './Title.jsx';
 import {useParams} from 'react-router-dom';
 import {MainContainer, FlexRow, SideContainer} from './Styled';
-import EventSiteButton from './EventSiteButton.jsx';
-import AttendButton from './AttendButton.jsx';
 import {getEvent} from './functions';
-
 
 const EventPage = () => {
   const [event, setEvent] = useState({});
@@ -28,26 +23,6 @@ const EventPage = () => {
       </FlexRow>
       <Description description={event.description || ''} />
     </MainContainer>
-    // <Container style={{maxWidth: '1500px'}}>
-    //   <Banner url={event.event_banner_url || '#'}/>
-    //   <div style={{
-    //     display: 'flex',
-    //     justifyContent: 'space-between',
-    //     alignItems: 'center',
-    //     width: '100%'}}
-    //   >
-    //     <Title info={event}></Title>
-    //     <ButtonHolder>
-    //       <AttendButton />
-    //       <CreateGroup event={event} />
-    //       <EventSiteButton url={event.link || ''}/>
-    //     </ButtonHolder>
-    //   </div>
-    //   <InfoContainer>
-    //     <Description description={event.description || ''} />
-    //     <SideInfo lineup={event.artist_list || [{}]} />
-    //   </InfoContainer>
-    // </Container>
   );
 };
 
